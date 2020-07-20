030ad5d187a3c04e4e004d41170798b442b7206f
Jens Schauder <jens@schauderhaft.de>
2020-07-20 11:41:00 +0200

    Initial commit.

---
+++ src/test/java/de/schauderhaft/javaaktuell/jpa/JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package de.schauderhaft.javaaktuell.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
